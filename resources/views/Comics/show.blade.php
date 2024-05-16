f895d1800932f32749cfeee57423d82a0ed8480d
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-16 19:20:25 +0200

    [fix]

--- resources/views/Comics/show.blade.php
+++ resources/views/Comics/show.blade.php
@@ -1,20 +1,20 @@
 @extends('layout.main')
 
 @section('content')
-  <div class="container my-5 ">
+  <div class="container show ">
     <div class="row">
       <div class="col">
         <img class=" show-img" src="{{ $comic->thumb }}" alt={{ $comic->title }}>
       </div>
       <div class="col">
-        <h5>Trama</h5>
+        <h3>Trama</h3>
         <p>{{ $comic->description }}</p>
-        <h5>Generi</h5>
+        <h3>Generi</h3>
         <p>{{ $comic->series }}</p>
-        <h5>Artisti e scrittori</h5>
+        <h3>Artisti e scrittori</h3>
         <p>{{ $comic->artists }}</p>
         <p>{{ $comic->writers }}</p>
-        <h5>Data di vendita</h5>
+        <h3>Data di vendita</h3>
         <p>{{ $comic->sale_date }}</p>
       </div>
     </div>
