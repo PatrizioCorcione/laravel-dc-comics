a6fe6a5a638cedf98187a2f6ef5177d515d83d1d
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-16 15:41:56 +0200

    [add] stamp on page

--- resources/views/Comics/index.blade.php
+++ resources/views/Comics/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layout.main')
 
 @section('content')
-  <h1>ciccio pasticcio</h1>
+  <h1>Fumettoni</h1>
+  <div class="container">
+    <div class="row">
+      @forelse ($comics as $item)
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img src="{{ $item->thumb }}" class="card-img-top" alt="{{ $item->title }}">
+            <div class="card-body">
+              <h5 class="card-title">{{ $item->title }}</h5>
+              <p class="card-text">{{ $item->type }}</p>
+              <p class="card-text">{{ $item->price }}</p>
+              <a href="#" class="btn btn-primary">vedi</a>
+              <a href="#" class="btn btn-secondary ">modifica</a>
+              <a href="#" class="btn btn-warning">cancella</a>
+            </div>
+          </div>
+        </div>
+      @empty
+      @endforelse
+    </div>
+  </div>
 @endsection
