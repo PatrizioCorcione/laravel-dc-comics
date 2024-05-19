a2878fe7d7b21be325e4c63477399d10f6bd6cbb
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-19 16:40:44 +0200

    [add] crousel video

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,56 @@
 @extends('layout.main')
 
 @section('content')
-  <div class="video-container">
-    <div class="video-overlay"></div>
-    <iframe width="795" height="395"
-      src="https://www.youtube.com/embed/uoOW_ggIEho?autoplay=1&loop=1&playlist=uoOW_ggIEho&mute=1&controls=0"
-      title="How Strong Is Cosmic Armor Superman?" frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="d-block w-100 ">
+          <div class="video-container">
+            <div class="video-overlay"></div>
+            <iframe width="795" height="395"
+              src="https://www.youtube.com/embed/uoOW_ggIEho?autoplay=1&loop=1&playlist=uoOW_ggIEho&mute=1&controls=0"
+              title="How Strong Is Cosmic Armor Superman?" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="container-fluid p-0">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="d-block w-100 ">
+          <div class="video-container">
+            <div class="video-overlay"></div>
+            <iframe width="702" height="395" src="https://www.youtube.com/embed/d2tAIiekEOA"
+              title="Peaceful Tavern Day - Relaxing Medieval Music, Fantasy Bard/Tavern Ambience, Celtic Music"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="container-fluid p-0">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="d-block w-100 ">
+          <div class="video-container">
+            <div class="video-overlay"></div>
+            <iframe width="702" height="395" src="https://www.youtube.com/embed/Ee9SqTIrVC4"
+              title="[Ghibli] Sentirsi Felici 🎵 6 Ore Di Musica Rilassante Da Ghibli Studio 🎵 Il Nome Della Vita, Teru"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
   </div>
 @endsection
